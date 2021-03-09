c558d8b17405dce8b4c2955ddf424beef2b8a0bf
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-03-09 17:16:56 -0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() => runApp(MultiProvider(
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //test CI con GitHubActions
     return MaterialApp(
       title: 'Proyectos Demo App',
       debugShowCheckedModeBanner: false,
