280ba176466e433919c68fae4d03d0853fb95d91
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-03-09 17:47:34 -0300

    remove import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 //import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:project_app/main.dart';
+//import 'package:project_app/main.dart';
 
 void main() {
   testWidgets('Default test', (WidgetTester tester) async {
