64a3ce623effaf5f9c34123e5652426b963196e9
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-03-09 20:03:15 -0300

    new default test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,17 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-//import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-//import 'package:project_app/main.dart';
+import 'package:project_app/main.dart';
 
 void main() {
-  testWidgets('Default test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    //await tester.pumpWidget(MyApp());
-
-    // // Verify that our counter starts at 0.
-    //expect(find.text('No hay proyectos aún'), findsNothing);
-    // expect(find.text('1'), findsNothing);
-
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+  group('Default tests', () {
+    testWidgets('Verificar que no hay proyectos cargados al iniciar',
+        (WidgetTester tester) async {
+      // Compila la app
+      await tester.pumpWidget(MyApp());
+      // Verifica que no hay proyectos
+      expect(find.text("No hay proyectos aún"), findsOneWidget);
+    });
   });
 }
