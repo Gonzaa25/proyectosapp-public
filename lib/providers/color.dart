6d8f1206e317046886541ef8ac4f744facd9590e
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-03-09 17:33:50 -0300

    new default color

--- lib/providers/color.dart
+++ lib/providers/color.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ColorThemeProvider extends ChangeNotifier {
-  MaterialColor? _color = Colors.teal;
+  MaterialColor? _color = Colors.green;
   MaterialColor? get appcolor => _color;
 
   void changeAppColor(MaterialColor? newColor) {
