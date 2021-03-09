a71bff3f01b82a6e3c4781d63316ec6d91194eed
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-03-09 17:44:43 -0300

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,12 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:project_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Default test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    //await tester.pumpWidget(MyApp());
 
     // // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    //expect(find.text('No hay proyectos aún'), findsNothing);
     // expect(find.text('1'), findsNothing);
 
     // // Tap the '+' icon and trigger a frame.
